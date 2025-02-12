06d9e0c42b7f768e045a5df1f5433dc86fda4959
loweryjoshua <52studiosCGI@gmail.com>
2025-02-12 15:44:17 -0600

    finishing up sliders

--- Exploratorium/Assets/Scripts/UpdateSliderText.cs
+++ Exploratorium/Assets/Scripts/UpdateSliderText.cs
@@ -7,16 +7,19 @@ public class UpdateSliderText : MonoBehaviour
     public TMP_Text text;
 
     public Slider slider;
+    public AK.Wwise.RTPC volumeRTPC;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         slider = GetComponent<Slider>();
         text.text += slider.value.ToString("#.00");
+        volumeRTPC.SetGlobalValue(slider.value);
     }
 
     public void UpdateText()
     {
         text.text = text.text.Split(" ")[0] + " " + text.text.Split(" ")[1] + " " + slider.value.ToString("#.00");
+        volumeRTPC.SetGlobalValue(slider.value);
     }
 }
