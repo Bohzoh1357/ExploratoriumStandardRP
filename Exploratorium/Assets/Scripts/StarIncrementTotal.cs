5d680e0192bf8e534a4ede57ed1d3ad01d8e382a
loweryjoshua <52studiosCGI@gmail.com>
2025-02-16 17:00:05 -0600

    everything but callbacks!

--- Exploratorium/Assets/Scripts/StarIncrementTotal.cs
+++ Exploratorium/Assets/Scripts/StarIncrementTotal.cs
@@ -1,3 +1,4 @@
+using StarterAssets;
 using UnityEngine;
 
 public class StarIncrementTotal : MonoBehaviour
@@ -6,18 +7,59 @@ public class StarIncrementTotal : MonoBehaviour
     public AK.Wwise.State starState;
     public AK.Wwise.Event collectionStinger;
 
+    public GameObject uiPickupCanvas;
+
+    public bool canPickup;
+
+    public float timeToDestroy;
+
     private void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        uiPickupCanvas = GameObject.Find("Canvas UI").transform.GetChild(6).gameObject;
+        canPickup = false;
     }
+
+    private void Update()
+    {
+        if (canPickup && Input.GetKeyDown(KeyCode.E))
+        {
+            gameManager.starIncrement();
+            starState.SetValue();
+            collectionStinger.Post(this.gameObject, (uint)AkCallbackType.AK_MusicSyncUserCue, callbackDestroy); // callback stuff here, make a function for it
+            uiPickupCanvas.SetActive(false);
+            canPickup = false;
+            GameObject.Destroy(gameObject, timeToDestroy);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            gameManager.starIncrement();
-            starState.SetValue();
-            collectionStinger.Post(this.gameObject);
-            GameObject.Destroy(gameObject);
+
+            // set canvas to active
+            uiPickupCanvas.gameObject.SetActive(true);
+            canPickup = true;
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            // set canvas to active
+            uiPickupCanvas.gameObject.SetActive(false);
+            canPickup = false;
+        }
+    }
+
+    void callbackDestroy(object in_cookie, AkCallbackType in_type, object in_info)
+    {
+        //GameObject.Destroy(gameObject);
+        Debug.Log("Entry Cue");
+    }
+
 }
