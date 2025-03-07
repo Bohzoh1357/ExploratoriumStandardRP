605135f25f379f2334f724faf4ee6fcd22cbff3a
loweryjoshua <52studiosCGI@gmail.com>
2025-03-06 21:10:10 -0600

    moved the soundbanks in Wwise and regenerated

---
+++ Exploratorium/Assets/StreamingAssets/Audio/Wwise_IDs.h
@@ -0,0 +1,297 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID ENDGAME = 4264815106U;
+        static const AkUniqueID FOOTSTEP = 1866025847U;
+        static const AkUniqueID INVENTORY_OPEN = 3325545974U;
+        static const AkUniqueID JUMP_LANDING = 2883244301U;
+        static const AkUniqueID MOUNTAIN_WIND1 = 1863533814U;
+        static const AkUniqueID MOUNTAIN_WIND2 = 1863533813U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID OCEANBACKGROUND = 644435109U;
+        static const AkUniqueID OCEANWAVES = 3350388417U;
+        static const AkUniqueID REGIONAMBIENCE = 3100698545U;
+        static const AkUniqueID RIVERCLOSE = 155135507U;
+        static const AkUniqueID RIVERFAR = 362167352U;
+        static const AkUniqueID RUNNINGWIND_PLAY = 1918324573U;
+        static const AkUniqueID RUNNINGWIND_STOP = 4104063435U;
+        static const AkUniqueID STARAMBIENT = 4106215671U;
+        static const AkUniqueID STARCOLLECTED = 1972369290U;
+        static const AkUniqueID STARINITIALIZER = 3219695003U;
+        static const AkUniqueID STOPALL = 3086540886U;
+        static const AkUniqueID STOPAMBIENTGLOBAL = 955784398U;
+        static const AkUniqueID STOPAMBIENTLOCAL = 3494773628U;
+        static const AkUniqueID STOPREGIONMUSIC = 2688098310U;
+        static const AkUniqueID TODAY = 1941965176U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace JUMP
+        {
+            static const AkUniqueID GROUP = 3833651337U;
+
+            namespace STATE
+            {
+                static const AkUniqueID GROUNDED = 2907122923U;
+                static const AkUniqueID INAIR = 1710892944U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace JUMP
+
+        namespace MUSIC
+        {
+            static const AkUniqueID GROUP = 3991942870U;
+
+            namespace STATE
+            {
+                static const AkUniqueID DAY = 311764537U;
+                static const AkUniqueID ENDGAME = 4264815106U;
+                static const AkUniqueID NIGHT = 1011622525U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace MUSIC
+
+        namespace REGIONS
+        {
+            static const AkUniqueID GROUP = 2091077764U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FIELD = 514358619U;
+                static const AkUniqueID FORREST = 760316600U;
+                static const AkUniqueID MOUNTAINTOP = 946954817U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OCEAN = 3802555985U;
+            } // namespace STATE
+        } // namespace REGIONS
+
+        namespace STAR0
+        {
+            static const AkUniqueID GROUP = 1281810867U;
+
+            namespace STATE
+            {
+                static const AkUniqueID COLLECTED = 3874392366U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID UNCOLLECTED = 395500817U;
+            } // namespace STATE
+        } // namespace STAR0
+
+        namespace STAR1
+        {
+            static const AkUniqueID GROUP = 1281810866U;
+
+            namespace STATE
+            {
+                static const AkUniqueID COLLECTED = 3874392366U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID UNCOLLECTED = 395500817U;
+            } // namespace STATE
+        } // namespace STAR1
+
+        namespace STAR2
+        {
+            static const AkUniqueID GROUP = 1281810865U;
+
+            namespace STATE
+            {
+                static const AkUniqueID COLLECTED = 3874392366U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID UNCOLLECTED = 395500817U;
+            } // namespace STATE
+        } // namespace STAR2
+
+        namespace STAR3
+        {
+            static const AkUniqueID GROUP = 1281810864U;
+
+            namespace STATE
+            {
+                static const AkUniqueID COLLECTED = 3874392366U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID UNCOLLECTED = 395500817U;
+            } // namespace STATE
+        } // namespace STAR3
+
+        namespace STAR4
+        {
+            static const AkUniqueID GROUP = 1281810871U;
+
+            namespace STATE
+            {
+                static const AkUniqueID COLLECTED = 3874392366U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID UNCOLLECTED = 395500817U;
+            } // namespace STATE
+        } // namespace STAR4
+
+        namespace STARID
+        {
+            static const AkUniqueID GROUP = 804627258U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID STAR0 = 1281810867U;
+                static const AkUniqueID STAR1 = 1281810866U;
+                static const AkUniqueID STAR2 = 1281810865U;
+                static const AkUniqueID STAR3 = 1281810864U;
+                static const AkUniqueID STAR4 = 1281810871U;
+            } // namespace STATE
+        } // namespace STARID
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace ENDGAMEBASS
+        {
+            static const AkUniqueID GROUP = 2472207775U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BASS2ND = 3331115756U;
+                static const AkUniqueID BASS4TH = 3163633552U;
+                static const AkUniqueID BASS8TH = 3164325180U;
+                static const AkUniqueID BASS8THT = 624393472U;
+                static const AkUniqueID BASS16TH = 608556873U;
+                static const AkUniqueID BASS16THT = 1660020639U;
+                static const AkUniqueID BASS32ND = 3836835385U;
+            } // namespace SWITCH
+        } // namespace ENDGAMEBASS
+
+        namespace JUMP
+        {
+            static const AkUniqueID GROUP = 3833651337U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GROUNDED = 2907122923U;
+                static const AkUniqueID INAIR = 1710892944U;
+            } // namespace SWITCH
+        } // namespace JUMP
+
+        namespace STARCOUNTER
+        {
+            static const AkUniqueID GROUP = 3156057335U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID STARSCOLLECT0 = 2268254018U;
+                static const AkUniqueID STARSCOLLECT1 = 2268254019U;
+                static const AkUniqueID STARSCOLLECT2 = 2268254016U;
+                static const AkUniqueID STARSCOLLECT3 = 2268254017U;
+                static const AkUniqueID STARSCOLLECT4 = 2268254022U;
+            } // namespace SWITCH
+        } // namespace STARCOUNTER
+
+        namespace STARID
+        {
+            static const AkUniqueID GROUP = 804627258U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID STAR0 = 1281810867U;
+                static const AkUniqueID STAR1 = 1281810866U;
+                static const AkUniqueID STAR2 = 1281810865U;
+                static const AkUniqueID STAR3 = 1281810864U;
+                static const AkUniqueID STAR4 = 1281810871U;
+            } // namespace SWITCH
+        } // namespace STARID
+
+        namespace STEP_PHYSICAL
+        {
+            static const AkUniqueID GROUP = 1817007209U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID ROCKS = 1863277117U;
+                static const AkUniqueID TERRAIN = 354267144U;
+            } // namespace SWITCH
+        } // namespace STEP_PHYSICAL
+
+        namespace STEP_TERRAIN
+        {
+            static const AkUniqueID GROUP = 3180743965U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID LEAVES = 582824249U;
+                static const AkUniqueID SNOW = 787898836U;
+            } // namespace SWITCH
+        } // namespace STEP_TERRAIN
+
+        namespace STEP_WATER
+        {
+            static const AkUniqueID GROUP = 901951531U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DRYLAND = 3451314871U;
+                static const AkUniqueID INWATER = 440928865U;
+            } // namespace SWITCH
+        } // namespace STEP_WATER
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID COLLECTION_SIDECHAIN = 2569537648U;
+        static const AkUniqueID DISTANCETOLASTPIECE = 1814698023U;
+        static const AkUniqueID MASTERVOLUME = 2918011349U;
+        static const AkUniqueID MUSICVOLUME = 2346531308U;
+        static const AkUniqueID SFXVOLUME = 988953028U;
+        static const AkUniqueID STARPIECES = 3570761712U;
+        static const AkUniqueID STINGER_SIDECHAIN = 4122641472U;
+        static const AkUniqueID TIMEOFDAY = 3729505769U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID STARCOLLECTED = 1972369290U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID GENERAL = 133642231U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID AMBIENT = 77978275U;
+        static const AkUniqueID COLLECTION = 2925776965U;
+        static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID STINGER = 78360149U;
+        static const AkUniqueID UI = 1551306167U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
